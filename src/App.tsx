d0c58ab23166e5c3a1e03f4688e2c39c8b538717
Schita01 <shakocitashvili6@gmail.com>
2024-03-17 13:27:57 +0400

    markup-for-calculator

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import TipCalculator from "./components/TipCalculator"
+
+
+function App() {
+
+  return (
+    <>
+      <div className="wrapper">
+        <div className="container">
+          <h2>
+            SPLI
+            <br />
+            TTER
+          </h2>
+          <TipCalculator/>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
